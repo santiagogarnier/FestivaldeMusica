c7d82c81e0146b237aa2e7e27304c135addb84fc
Santi Garnier <108945065+santiagogarnier@users.noreply.github.com>
2026-03-01 19:31:03 -0300

    cambios en el scroll

--- build/js/app.js
+++ build/js/app.js
@@ -1,19 +1,121 @@
-document.addEventListener('DOMContentLoaded',function(){
- crearGaleria();
+document.addEventListener('DOMContentLoaded', function () {
+
+    navegacionFija()
+    crearGaleria()
+    resaltarEnlace()    
+    scrollNav()
 })
 
-function crearGaleria(){
+function navegacionFija(){
+    const header = document.querySelector('.header')
+    const sobreFestival = document.querySelector('.sobre-festival')
+
+    document.addEventListener('scroll', function(){
+            if(sobreFestival.getBoundingClientRect().bottom < 1){
+                header.classList.add('fixed')
+            }else{
+                header.classList.remove('fixed')
+            }
+    })
+}
+
+function crearGaleria() {
     const galeria = document.querySelector('.galeria-imagenes')
 
-    const cantidadImagenes = 16;
+    const cantidadImagenes = 16
 
-
-    for(let i = 1; i <= cantidadImagenes ; i++){
+    for (let i = 1; i <= cantidadImagenes; i++) {
         const imagen = document.createElement('IMG')
         imagen.src = `src/img/gallery/full/${i}.jpg`
-        imagen.alt = 'Imagen Galeria'
+        imagen.accessKeylt = 'Imagen Galeria'
+
+        //Event Handler: proceso de detectar y responder a la interaccion del usuario
+        imagen.onclick = function () {
+            mostrarImagen(i)
+        }
+
 
         galeria.appendChild(imagen)
     }
+}
 
+function mostrarImagen(i) {
+
+    const imagen = document.createElement('IMG')
+    imagen.src = `src/img/gallery/full/${i}.jpg`
+    imagen.accessKeylt = 'Imagen Galeria'
+
+
+    console.log('Desde mostrarImagen', i)
+
+    //Generar Modal
+    const modal = document.createElement('DIV')
+    modal.classList.add('modal')
+    modal.onclick = cerrarModal
+    modal.appendChild(imagen)
+
+    //Boton cerrar modal
+    const cerrarModalBtn = document.createElement('BUTTON')
+    cerrarModalBtn.textContent = 'X'
+    cerrarModalBtn.classList.add('btn-cerrar')
+    cerrarModalBtn.onclick = cerrarModal
+
+    modal.appendChild(imagen)
+    modal.appendChild(cerrarModalBtn)
+    //agregar al HTML
+    const body = document.querySelector('body')
+    body.classList.add('overflow-hidden')
+    body.appendChild(modal)
+
+}
+
+function cerrarModal() {
+    const modal = document.querySelector('.modal')
+    modal.classList.add('fade-out')
+    modal.classList.add('fade-out')
+
+    setTimeout(() => {
+        modal?.remove()
+
+        const body = document.querySelector('body')
+        body.classList.remove('overflow-hidden')
+    }, 500);
+
+}
+
+function resaltarEnlace() {
+    document.addEventListener('scroll', function(){
+        const sections  = document.querySelectorAll('section')
+        const navLinks = document.querySelectorAll ('.navegacion-principal a')
+
+        let actual = ''
+        sections.forEach (section => {
+            const sectionTop = section.offsetTop;
+            const sectionHeight = section.clientHeight;
+
+            if(window.scrollY >= (sectionTop - sectionHeight / 3)){
+                actual = section.id
+            }
+
+        })
+        navLinks.forEach(link => {
+            if(link.getAttribute('href') === '#' + actual){
+                link.classList.add('active')
+            }
+        })
+    })
+}
+
+function scrollNav(){
+    const navLinks = document.querySelectorAll('.navegacion-principal a')
+    
+    navLinks.forEach( link => {
+        link.addEventListener('click', e =>{
+            e.preventDefault()
+            const sectionScroll  = e.target.getAttribute('href')
+            const section = document.querySelector(sectionScroll)
+
+            section.scrollIntoView({behavior: 'smooth'})             
+        })
+    })
 }
